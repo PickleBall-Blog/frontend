701cc96ea9170c3eb5cc3216e84f3595894975fd
Tuan147 <90153951+Tuan147@users.noreply.github.com>
2023-01-24 08:57:24 -0500

    created react app and started on card component skeleton

---
+++ frontend/src/components/Card.js
@@ -0,0 +1,13 @@
+export default function Card() {
+
+    return (
+        <>
+        <h4 className="card-source">Source</h4>
+        <h2 className="card-title">Title</h2>
+        <h5 className="card-category">Category</h5>
+        <p className="card-author">James Gun</p>
+        <img src="" alt="" className="card-thumbnail" />
+        </>
+    )
+
+};
